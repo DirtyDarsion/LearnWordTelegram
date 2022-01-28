16c84c8e48f39e29e00a52ea24e1c67708d8ead9
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-01-28 22:43:26 +0600

    Fix contacts, add model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=30)
+    text = models.TextField()
+    # img_url = models.ImageField(upload_to='mainapp/media/products/', width_field=None)
+    price = models.IntegerField(default=True)
+
+    def __str__(self):
+        return self.name
--- sharhouse55/urls.py
+++ sharhouse55/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('mainapp.urls'), name='mainapp'),
+    path('', include('mainapp.urls'), name='index'),
     path('admin-panel/', admin.site.urls),
 ]
