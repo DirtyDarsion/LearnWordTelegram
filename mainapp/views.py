82ab7b67f6d96695563239abfcc34bba8f51cfdc
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-01-29 01:07:11 +0600

    Learn env

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,11 @@
+import os
 from django.shortcuts import render
 from .models import Products
 
 
 def index(request):
     data = {
-        'Products': Products.objects.filter(show_in_index=True)
+        'Products': Products.objects.filter(show_in_index=True),
+        'KEY': os.getenv('KEY')
     }
     return render(request, 'mainapp/content.html', data)
