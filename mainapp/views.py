5393990fe6629672f4fea5b84182ce379e3a373b
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-01-29 01:14:16 +0600

    Learn env

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,3 @@
-import os
 from django.shortcuts import render
 from .models import Products
 
@@ -6,6 +5,5 @@ from .models import Products
 def index(request):
     data = {
         'Products': Products.objects.filter(show_in_index=True),
-        'KEY': os.getenv('KEY')
     }
     return render(request, 'mainapp/content.html', data)
--- sharhouse55/settings.py
+++ sharhouse55/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-23jza1x+nv__^_a!d&0gil^u6--nnqf+$zf9=x6=!6iq1j%)-5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
