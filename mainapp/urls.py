97e3cf08ad1341a857164d488ec27df85dafe4ef
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-02-02 03:08:26 +0600

    Add products page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, test
+from .views import IndexListView, ProductsListView, test
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', IndexListView.as_view(), name='index'),
+    path('products', ProductsListView.as_view(), name='products'),
     path('test', test, name='test')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Products
 
 
-def index(request):
-    data = {
-        'Products': Products.objects.filter(show_in_index=True),
-    }
-    return render(request, 'mainapp/content.html', data)
+class IndexListView(generic.ListView):
+    model = Products
+    context_object_name = 'products'
+    queryset = Products.objects.filter(show_in_index=True)
+    template_name = 'mainapp/content.html'
+
+
+class ProductsListView(generic.ListView):
+    model = Products
+    context_object_name = 'products'
+    template_name = 'mainapp/products.html'
 
 
 def test(request):
