0c578ddeb2f9176eb12eb29d3e7ded73ce656521
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-02-07 22:59:15 +0600

    Add images of categories

--- sharhouse55/settings.py
+++ sharhouse55/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv('DEBUG', 1))
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'sharhouse55.herokuapp.com', '192.168.0.6']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'sharhouse55.herokuapp.com', '192.168.0.6', '192.168.0.10']
 
 
 # Application definition
