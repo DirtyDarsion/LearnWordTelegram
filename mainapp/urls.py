9de4bffde458c3c00b90c4ba8035a7fdf26479dc
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-02-07 15:28:07 +0600

    Add categories to view

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,6 +5,7 @@ from .views import IndexListView, ProductsListView, care, qr_code, qr_code1
 urlpatterns = [
     path('', IndexListView.as_view(), name='index'),
     path('products', ProductsListView.as_view(), name='products'),
+    path('products/<int:pk>/', ProductsListView.as_view(), name='category_products'),
     path('care', care, name='care'),
     path('covid-cert/status/c4530ceb-7a4a-475c-bab2-882ees8b50ad', qr_code, name='qr_code'),
     path('covid-cert/status/c4530ceb-7a4a-475c-bab2-882ees8b50ab', qr_code1, name='qr_code'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Products
+from .models import Products, Category
 
 
 class IndexListView(generic.ListView):
@@ -24,6 +24,17 @@ class ProductsListView(generic.ListView):
     model = Products
     context_object_name = 'products'
     template_name = 'mainapp/products.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+    def get_queryset(self):
+        if 'pk' in self.kwargs:
+            return Products.objects.filter(category=self.kwargs['pk'])
+        else:
+            return Products.objects.all()
 
 
 def care(request):
