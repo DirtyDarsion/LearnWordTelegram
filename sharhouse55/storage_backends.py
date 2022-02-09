a2b20ed0fd6ca1600a1396169ddc8d45cbbecf5a
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-02-09 20:28:12 +0600

    Minor changes

--- sharhouse55/storage_backends.py
+++ sharhouse55/storage_backends.py
@@ -1,10 +1,10 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-THUMBNAIL_FORCE_OVERWRITE = True
-
-
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
+
+
+THUMBNAIL_FORCE_OVERWRITE = True
