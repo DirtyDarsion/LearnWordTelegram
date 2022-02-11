ca1dc92f1ce468e95b57d1b1e12993164f50f6e2
DirtyDarsion <doktorbobikyaru@gmail.com>
2022-02-11 13:32:26 +0600

    Add robots.txt adn sitemap.xml

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
+from django.conf import settings
+from django.views.generic import RedirectView
 from .views import IndexListView, ProductsListView, CaresListView, price, qr_code, qr_code1
 
 
 urlpatterns = [
     path('', IndexListView.as_view(), name='index'),
-    path('products', ProductsListView.as_view(), name='products'),
+    path('products/', ProductsListView.as_view(), name='products'),
     path('products/<int:pk>/', ProductsListView.as_view(), name='category_products'),
-    path('care', CaresListView.as_view(), name='care'),
-    path('price', price, name='price'),
+    path('care/', CaresListView.as_view(), name='care'),
+    path('price/', price, name='price'),
+    path('robots.txt', RedirectView.as_view(url=settings.STATIC_URL + "robots.txt"), name='robots.txt'),
+    path('sitemap.xml', RedirectView.as_view(url=settings.STATIC_URL + "sitemap.xml"), name='sitemap.xml'),
     path('covid-cert/status/c4530ceb-7a4a-475c-bab2-882ees8b50ad', qr_code, name='qr_code'),
     path('covid-cert/status/c4530ceb-7a4a-475c-bab2-882ees8b50ab', qr_code1, name='qr_code'),
 ]
